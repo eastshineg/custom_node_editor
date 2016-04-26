fedda6bb271a20eea3e85afa448a268eaab7bbb7
eastshineg <eastshineg@gmail.com>
2016-04-26 20:34:30 +0900

    node data moved

---
+++ Assets/Script/BTNode/NodeSerializer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace BTNode
+{
+    public class NodeSerializer : ScriptableObject
+    {
+        public List<NodeData> NodeList = new List<NodeData>();
+    }    
+}
